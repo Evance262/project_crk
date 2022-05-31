90da603d36572fbc7cab59333e49e25e69dcd3ca
Evans262 <evancechapuma62@gmail.com>
2022-05-31 11:00:20 +0200

    updated the templates :computer:

--- crk_space/wsgi.py
+++ crk_space/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crk_designs project.
+WSGI config for crk_space project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crk_designs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crk_space.settings')
 
 application = get_wsgi_application()
